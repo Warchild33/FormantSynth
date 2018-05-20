d3b8d42e7e7d2607ee9da6fb68470d97f37bc762
Vladimir <vdbarster@gmail.com>
2018-05-20 11:53:30 +0300

    freq sliders rotation

--- formantsynthsvg.cpp
+++ formantsynthsvg.cpp
@@ -1,4 +1,5 @@
 #include <QTransform>
+#include <QtMath>
 #include "formantsynthsvg.h"
 
 FormantSynthSvg::FormantSynthSvg(QWidget *parent) : SvgWidget(parent)
@@ -17,7 +18,13 @@ FormantSynthSvg::FormantSynthSvg(QWidget *parent) : SvgWidget(parent)
     for(int i=1; i < 10; i++)
     {
         QString name = QString("F%1").arg(i);
-        freqSliders[i] = Slider(getPath(name).boundingRect(), name);
+        QRectF rect = getPath(name).boundingRect();
+        float w = rect.width();
+        rect.setX(rect.center().x() - w );
+        rect.setY(rect.center().y() - w );
+        rect.setWidth(w*2);
+        rect.setHeight(w*2);
+        freqSliders[i] = Slider(rect, name);
     }
     setFocus();
 
@@ -41,16 +48,40 @@ void  FormantSynthSvg::mousePressEvent(QMouseEvent* event)
 
 }
 
+double findNearestPointOnCircle(QPointF mouse, QPointF center, float R)
+{
+    double  X,Y,d, minD=100000000;
+    double  min_angle;
+    for(double angle=0; angle < 360; angle+=1)
+    {
+        X = center.x() + R * cos( qDegreesToRadians(angle) );
+        Y = center.y() + R * sin( qDegreesToRadians(angle) );
+        d = (mouse.x() - X)*(mouse.x() - X) + (mouse.y() - Y)*(mouse.y() - Y);
+        if(minD!=qMin(d, minD))
+            min_angle = angle;
+        minD = qMin(d, minD);
+    }
+    return 270 - min_angle;
+}
+
 void  FormantSynthSvg::mouseMoveEvent(QMouseEvent* event)
 {
+    float angle;
     for(int i=0; i < 10; i++)
     {
         QPoint pos = event->pos();
         pos.setY(height() - pos.y());
         //fprintf(stderr,"mouse %d %d\n", pos.x(), pos.y());
         QPointF p = mapToViewbox(pos, viewbox);
+        angle = findNearestPointOnCircle(p, freqSliders[i].rect.center(),
+                                            freqSliders[i].rect.width());
+        //fprintf(stderr,"angle %f\n", angle);
+
         if( freqSliders[i].rect.contains(p) )
+        {
             setStroke(freqSliders[i].name, freqSliders[i].activeColor);
+            rotateNode(freqSliders[i].name, freqSliders[i].rect.center(), angle);
+        }
         else
             setStroke(freqSliders[i].name, freqSliders[i].defaultColor);
     }
