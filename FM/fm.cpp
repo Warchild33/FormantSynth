a2564608c95f17603a400aec79438615affeca2e
Vladimir <vdbarster@gmail.com>
2019-04-04 17:10:46 +0300

    optimized sin by table

--- FM/fm.cpp
+++ FM/fm.cpp
@@ -8,11 +8,15 @@
 double algo0(double A, double f_oc, double fm1, double fm2, double I1, double I2)
 {
     double x;
-    x = A*sin(2 * M_PI * f_oc * t + I1*sin( 2 * M_PI * fm1 * t ) + I2*sin(2 * M_PI * fm2 * t)  );
+    x = A*sinT(2 * M_PI * f_oc * t + I1*sinT( 2 * M_PI * fm1 * t ) + I2*sinT(2 * M_PI * fm2 * t)  );
     return x;
 }
 */
 extern Ploter* p;
+
+double sine_table[1000] = {0};
+
+inline double sinT(double angle_rad);
 
 FMSynth::FMSynth()
 :Syntezer()
@@ -20,7 +24,48 @@ FMSynth::FMSynth()
     TestEvenlope();
     n_test = 1;
 
+    //generate sin table
+    for (int i = 1; i <= 1000; i++)
+    {
+        sine_table[i-1] = std::sin(2 * M_PI * i/ 1000.0);
+    }
+    sinT( -4*M_PI );
+    sinT( 4*M_PI );
 }
+
+inline double sinT(double angle_rad)
+{
+    if(angle_rad!=angle_rad)
+        return 0;
+
+    if( angle_rad < 0 )
+    {
+        angle_rad+=int(abs(angle_rad) / (2 * M_PI)) * 2 * M_PI + (2 * M_PI);
+    }
+
+    if( angle_rad > (2 * M_PI) )
+    {
+        angle_rad-=int(abs(angle_rad) / (2 * M_PI)) * 2 * M_PI ;
+    }
+   // qDebug() << "angle_rad=" << angle_rad;
+   //return 0;
+
+   int i = (angle_rad/(2*M_PI)) * 1000.0;
+   return sine_table[i];
+}
+
+//inline double sinT(double angle)
+//{
+//    double x5=angle,x3=angle,x7=angle;
+//    for(int i=0; i < 3; i++ )
+//        x3*=x3;
+//    for(int i=0; i < 5; i++ )
+//        x5*=x5;
+//    for(int i=0; i < 7; i++ )
+//        x7*=x7;
+//    value = angle - x3/6 + x5/120 - x7/5040;
+//    return value;
+//}
 
 void FMSynth::TestEvenlope()
 {
@@ -96,8 +141,8 @@ double FMSynth::Evenlope(FmParams* params, double t)
 //testing feedback loop
 double FMSynth::algo17(FmParams* p, double t)
 {
-    p->out[2] = p->I[2] * sin( 2 * M_PI * p->f[2] * t + p->out[2] );
-    return p->I[1] * sin( 2 * M_PI * p->f[1] * t + p->out[2]);
+    p->out[2] = p->I[2] * sinT( 2 * M_PI * p->f[2] * t + p->out[2] );
+    return p->I[1] * sinT( 2 * M_PI * p->f[1] * t + p->out[2]);
 }
 
 
@@ -113,8 +158,9 @@ double* FMSynth::Test1(double f_oc=800, double SampleRate=48000, double time=2, 
     double t = 0;
     double dt = 1. / SampleRate;
     p->clearvals(0);
+
     for(int n=0; n < floor(time*SampleRate); n++) //
-    {
+    {        
         x[n] = algo17(&param, t);
         t+=dt;
         if(n < 1000)
@@ -127,7 +173,7 @@ double* FMSynth::Test1(double f_oc=800, double SampleRate=48000, double time=2, 
 
 double amp_mod(double Amax, double fvib, double t)
 {
-    return Amax * sin( 2 * M_PI * fvib * t);
+    return Amax * sinT( 2 * M_PI * fvib * t);
 }
 
 //testing algo 19
@@ -142,12 +188,12 @@ double FMSynth::algo19(FmParams* p, double t, int n)
    // qDebug() << d[6];
 
     double ev = Evenlope(p, t);
-    p->out[6] = p->I[6] * sin( 2 * M_PI * (p->f[6]) * t + d[6]*p->out[6]);
-    p->out[4] = ev * p->I[4] * sin( 2 * M_PI * (p->f[4]) * t + d[4]*p->out[6]);
-    p->out[5] = ev * p->I[5] * sin( 2 * M_PI * (p->f[5]) * t + d[5]*p->out[6]);
-    p->out[3] = p->I[3] * sin( 2 * M_PI * p->f[3] * t );
-    p->out[2] = p->I[2] * sin( 2 * M_PI * p->f[2] * t + p->out[3] ) ;
-    p->out[1] = ev *p->I[1] * sin( 2 * M_PI * p->f[1] * t + p->out[2]);
+    p->out[6] = p->I[6] * sinT( 2 * M_PI * (p->f[6]) * t + d[6]*p->out[6]);
+    p->out[4] = ev * p->I[4] * sinT( 2 * M_PI * (p->f[4]) * t + d[4]*p->out[6]);
+    p->out[5] = ev * p->I[5] * sinT( 2 * M_PI * (p->f[5]) * t + d[5]*p->out[6]);
+    p->out[3] = p->I[3] * sinT( 2 * M_PI * p->f[3] * t );
+    p->out[2] = p->I[2] * sinT( 2 * M_PI * p->f[2] * t + p->out[3] ) ;
+    p->out[1] = ev *p->I[1] * sinT( 2 * M_PI * p->f[1] * t + p->out[2]);
     return p->out[1] + p->out[4] + p->out[5];
 }
 
@@ -167,10 +213,10 @@ double* FMSynth::Test2(double f_oc, double SampleRate, double time, int* N, bool
     param.level[2]=0.8;   //decay
     param.level[3]=0.7;   //sustain
     param.level[4]=0.1;   //release
-    param.rate[0]=1;    //attack time
-    param.rate[1]=1;    //decay time
+    param.rate[0]=0.5;    //attack time
+    param.rate[1]=0.5;    //decay time
     param.rate[2]=10;      //sustain time
-    param.rate[3]=1;    //release time
+    param.rate[3]=0.5;    //release time
 
     double* x = zeroes(0, floor(time*SampleRate));
     double t = 0;
@@ -178,7 +224,7 @@ double* FMSynth::Test2(double f_oc, double SampleRate, double time, int* N, bool
     {
         t = t_last;
         param.rate[2]=0.00001+t_last-param.rate[0]-param.rate[1];      //sustain time
-        param.rate[3]=1;    //release time
+        param.rate[3]=0.5;    //release time
 
     }
     double dt = 1. / SampleRate;
@@ -187,9 +233,9 @@ double* FMSynth::Test2(double f_oc, double SampleRate, double time, int* N, bool
     {
         x[n] = algo19(&param, t, n);
         t+=dt;
-        if(n % 100 == 0)
+        //if(n % 100 == 0)
         //if(n < 100)
-          p->setXY(0, t-t_last, x[n]);
+        //  p->setXY(0, t-t_last, x[n]);
 
     }
     p->autoscale = true;
@@ -202,12 +248,12 @@ double* FMSynth::Test2(double f_oc, double SampleRate, double time, int* N, bool
 
 //double FMSynth::algo1(FmParams* p, double t)
 //{
-//    double OP6 = p->I[6] * sin( 2 * M_PI * p->f[6] * t +  p->I[6] *sin( 2 * M_PI * p->f[6] * t ) );
-//    double OP5 = p->I[5] * sin( 2 * M_PI * p->f[5] * t );
-//    double OP4 = p->I[4] * sin( 2 * M_PI * p->f[4] * t );
-//    double OP3 = p->I[3] * sin( 2 * M_PI * p->f[3] * t + OP4 + OP5 + OP6 );
-//    double OP2 = p->I[2] * sin( 2 * M_PI * p->f[2] * t );
-//    double OP1 = p->I[1] * sin( 2 * M_PI * p->f[1] * t + OP2);
+//    double OP6 = p->I[6] * sinT( 2 * M_PI * p->f[6] * t +  p->I[6] *sinT( 2 * M_PI * p->f[6] * t ) );
+//    double OP5 = p->I[5] * sinT( 2 * M_PI * p->f[5] * t );
+//    double OP4 = p->I[4] * sinT( 2 * M_PI * p->f[4] * t );
+//    double OP3 = p->I[3] * sinT( 2 * M_PI * p->f[3] * t + OP4 + OP5 + OP6 );
+//    double OP2 = p->I[2] * sinT( 2 * M_PI * p->f[2] * t );
+//    double OP1 = p->I[1] * sinT( 2 * M_PI * p->f[1] * t + OP2);
 //    return OP1 + OP3;
 
 //}
